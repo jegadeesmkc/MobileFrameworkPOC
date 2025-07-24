b3b3374d65cc0a71c2f19131a70e52f985bd87f7
Jegadeeswaran <jegadeesmkce@gmail.com>
2025-07-24 23:44:42 +0530

    add framework

---
+++ MobileFrameworkPOC/MobileFrameworkPOC/MobileFrameworkPOC.h
@@ -0,0 +1,18 @@
+//
+//  MobileFrameworkPOC.h
+//  MobileFrameworkPOC
+//
+//  Created by Jegadeeswaran on 24/07/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MobileFrameworkPOC.
+FOUNDATION_EXPORT double MobileFrameworkPOCVersionNumber;
+
+//! Project version string for MobileFrameworkPOC.
+FOUNDATION_EXPORT const unsigned char MobileFrameworkPOCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileFrameworkPOC/PublicHeader.h>
+
+
---
+++ MobileFrameworkPOC/MobileNumberValidation.swift
@@ -0,0 +1,33 @@
+//
+//  MobileNumberValidation.swift
+//  MobileFrameworkPOC
+//
+//  Created by Jegadeeswaran on 24/07/25.
+//
+
+import Foundation
+
+public class MobileNumberValidator {
+    
+    public init() {}
+    
+    public func validate(mobile: String?) -> ValidationResult {
+        guard let mobile = mobile?.trimmingCharacters(in: .whitespacesAndNewlines), !mobile.isEmpty else {
+            return .failure("Mobile number cannot be empty")
+        }
+        
+        let regex = "^[0-9]{10}$"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
+        
+        if predicate.evaluate(with: mobile) {
+            return .success
+        } else {
+            return .failure("Invalid mobile number format. It should be exactly 10 digits.")
+        }
+    }
+}
+
+public enum ValidationResult {
+    case success
+    case failure(String)
+}
